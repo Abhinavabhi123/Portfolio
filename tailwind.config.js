08ff346860c920ac0b34c8666674f1090915f62c
Abhinav <abhinavkinarullathil@gmail.com>
2023-09-08 18:19:20 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'primary-blue': '##0ea5e9', 
+    },
+  },
+  plugins: [],
+}
